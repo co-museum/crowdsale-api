a77a5534d2d3a4f8697566ebd6758026134c43ed
Uros Perisic <uros.m.perisic@gmail.com>
2022-08-17 20:24:28 +0800

    Start using dependency injection

--- functions/src/handlers/admin.ts
+++ functions/src/handlers/admin.ts
@@ -1,7 +1,7 @@
 import * as logger from "firebase-functions/lib/logger";
 import {Record, Number, String, Array, Static} from "runtypes";
 import {Request, Response} from "express";
-import {getFirestore} from "firebase-admin/firestore";
+import {Firestore} from "firebase-admin/firestore";
 import {union, difference} from "lodash";
 import {StatusCodes} from "http-status-codes";
 import {ethers} from "ethers";
@@ -77,108 +77,121 @@ function validateAddresses(handler: Handler, addresses: string[], res: Response)
 }
 
 /**
- * adds entire whitelist document to firestore
- * @param {Request<Params>} req the whitelist to add into specified batch under specified name
- * @param {Response<Whitelist>} res the whitelist actually written to the db
- **/
-export async function addWhitelist(
-    req: Request<Params>,
-    res: Response
-) {
-  try {
-    logRequest(Handler.AddWhitelist, req.params, req.body);
-    Params.check(req.params);
-    Whitelist.check(req.body);
-    const whitelist: Whitelist = req.body;
-    validateAddresses(Handler.AddWhitelist, whitelist.addresses, res);
-
-    const db = getFirestore();
-    const ref = db.collection(req.params.batch).doc(req.params.whitelist);
-    await ref.set(whitelist);
-    const document = await ref.get();
-    res.status(StatusCodes.OK).json(document.data());
-  } catch (err) {
-    handleError(Handler.AddWhitelist, err, res);
+ * Admin handler class
+ */
+export class Admin {
+  db: Firestore;
+  /**
+   * Dependency injection point
+   * @param {Firestore} db firestore instance
+   */
+  constructor(db: Firestore) {
+    this.db = db;
   }
-}
 
-/**
- * remove entire whitelist document from
- * @param {Request<Params>} req the whitelist to add into specified batch under specified name
- * @param {Response} res params echoed back if OK
- **/
-export async function removeWhitelist(
-    req: Request<Params>,
-    res: Response
-) {
-  try {
-    logRequest(Handler.RemoveWhitelist, req.params, req.body);
-    Params.check(req.params);
+  /**
+   * adds entire whitelist document to firestore
+   * @param {Request<Params>} req the whitelist to add into specified batch under specified name
+   * @param {Response<Whitelist>} res the whitelist actually written to the db
+   **/
+  async addWhitelist(
+      req: Request<Params>,
+      res: Response
+  ) {
+    try {
+      logRequest(Handler.AddWhitelist, req.params, req.body);
+      Params.check(req.params);
+      Whitelist.check(req.body);
+      const whitelist: Whitelist = req.body;
+      validateAddresses(Handler.AddWhitelist, whitelist.addresses, res);
 
-    const db = getFirestore();
-    const ref = db.collection(req.params.batch).doc(req.params.whitelist);
-    await ref.delete();
-    res.status(StatusCodes.OK).json(req.params);
-  } catch (err) {
-    handleError(Handler.RemoveWhitelist, err, res);
+      const ref = this.db.collection(req.params.batch).doc(req.params.whitelist);
+      await ref.set(whitelist);
+      const document = await ref.get();
+      res.status(StatusCodes.OK).json(document.data());
+    } catch (err) {
+      handleError(Handler.AddWhitelist, err, res);
+    }
   }
-}
 
-/**
- * adds addresses in body to specified whitelist
- * @param {Request<Params>} req
- * @param {Response} res the current whitelist in db if OK
- **/
-export async function addAddresses(
-    req: Request<Params>,
-    res: Response,
-) {
-  try {
-    logRequest(Handler.AddAddresses, req.params, req.body);
-    Params.check(req.params);
-    logger.log("params ok");
-    Addresses.check(req.body);
-    validateAddresses(Handler.AddAddresses, req.body, res);
+  /**
+   * remove entire whitelist document from
+   * @param {Request<Params>} req the whitelist to add into specified batch under specified name
+   * @param {Response} res params echoed back if OK
+   **/
+  async removeWhitelist(
+      req: Request<Params>,
+      res: Response
+  ) {
+    try {
+      logRequest(Handler.RemoveWhitelist, req.params, req.body);
+      Params.check(req.params);
 
-    const db = getFirestore();
-    const ref = db.collection(req.params.batch).doc(req.params.whitelist);
-    const old = await (await ref.get()).data() as Whitelist;
-    // HACK: the firestore arrayUnion and arrayRemove are broken
-    const whitelist = {addresses: union(old.addresses, req.body)};
-    await ref.update(whitelist);
-    const updated = await ref.get();
-    res.status(StatusCodes.OK).json(updated.data());
-  } catch (err) {
-    handleError(Handler.AddAddresses, err, res);
+      const ref = this.db.collection(req.params.batch).doc(req.params.whitelist);
+      await ref.delete();
+      res.status(StatusCodes.OK).json(req.params);
+    } catch (err) {
+      handleError(Handler.RemoveWhitelist, err, res);
+    }
   }
-}
 
-/**
- * removes addresses in body from specified whitelist
- * @param {Request<Params>} req
- * @param {Response} res the current whitelist in db if OK
- **/
-export async function removeAddresses(
-    req: Request<Params>,
-    res: Response,
-) {
-  try {
-    logRequest(Handler.RemoveAddresses, req.params, req.body);
-    Params.check(req.params);
-    Addresses.check(req.body);
-    validateAddresses(Handler.RemoveAddresses, req.body, res);
+  /**
+   * adds addresses in body to specified whitelist
+   * @param {Request<Params>} req
+   * @param {Response} res the current whitelist in db if OK
+   **/
+  async addAddresses(
+      req: Request<Params>,
+      res: Response,
+  ) {
+    try {
+      logRequest(Handler.AddAddresses, req.params, req.body);
+      Params.check(req.params);
+      Addresses.check(req.body);
+      validateAddresses(Handler.AddAddresses, req.body, res);
+      logger.log("addresses ok");
+      logger.log("db", {foo: this.db});
 
-    const db = getFirestore();
-    const ref = db.collection(req.params.batch).doc(req.params.whitelist);
-    const old = await (await ref.get()).data() as Whitelist;
-    // HACK: the firestore arrayUnion and arrayRemove are broken
-    logger.log(old);
-    const whitelist = {addresses: difference(old.addresses, req.body)};
-    logger.log(whitelist);
-    await ref.update(whitelist);
-    const updated = await ref.get();
-    res.status(StatusCodes.OK).json(updated.data());
-  } catch (err) {
-    handleError(Handler.RemoveAddresses, err, res);
+      const ref = this.db.collection(req.params.batch).doc(req.params.whitelist);
+      logger.log("ref ok");
+      const old = await (await ref.get()).data() as Whitelist;
+      logger.log("old ok");
+      // HACK: the firestore arrayUnion and arrayRemove are broken
+      const whitelist = {addresses: union(old.addresses, req.body)};
+      logger.log("union ok");
+      await ref.update(whitelist);
+      logger.log("update ok");
+      const updated = await ref.get();
+      res.status(StatusCodes.OK).json(updated.data());
+    } catch (err) {
+      handleError(Handler.AddAddresses, err, res);
+    }
+  }
+
+  /**
+   * removes addresses in body from specified whitelist
+   * @param {Request<Params>} req
+   * @param {Response} res the current whitelist in db if OK
+   **/
+  async removeAddresses(
+      req: Request<Params>,
+      res: Response,
+  ) {
+    try {
+      logRequest(Handler.RemoveAddresses, req.params, req.body);
+      Params.check(req.params);
+      Addresses.check(req.body);
+      validateAddresses(Handler.RemoveAddresses, req.body, res);
+
+      const ref = this.db.collection(req.params.batch).doc(req.params.whitelist);
+      const old = await (await ref.get()).data() as Whitelist;
+      // HACK: the firestore arrayUnion and arrayRemove are broken
+      const whitelist = {addresses: difference(old.addresses, req.body)};
+      await ref.update(whitelist);
+      const updated = await ref.get();
+      res.status(StatusCodes.OK).json(updated.data());
+    } catch (err) {
+      handleError(Handler.RemoveAddresses, err, res);
+    }
   }
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,20 @@
 import * as functions from "firebase-functions";
-import * as adminHandlers from "./handlers/admin";
+import {Admin} from "./handlers/admin";
 import express from "express";
 import {initializeApp} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
 
 const app = express();
 app.use(express.json());
 initializeApp();
 
-app.put("/admin/whitelist/:batch/:whitelist", adminHandlers.addWhitelist);
-app.delete("/admin/whitelist/:batch/:whitelist", adminHandlers.removeWhitelist);
-app.put("/admin/address/:batch/:whitelist", adminHandlers.addAddresses);
-app.delete("/admin/address/:batch/:whitelist", adminHandlers.removeAddresses);
+const db = getFirestore();
+const admin = new Admin(db);
+
+app.put("/admin/whitelist/:batch/:whitelist", admin.addWhitelist.bind(admin));
+app.delete("/admin/whitelist/:batch/:whitelist", admin.removeWhitelist.bind(admin));
+app.put("/admin/address/:batch/:whitelist", admin.addAddresses.bind(admin));
+app.delete("/admin/address/:batch/:whitelist", admin.removeAddresses.bind(admin));
 
 export const api = functions
     .region("asia-southeast1") // comma separated string to multiple regions
