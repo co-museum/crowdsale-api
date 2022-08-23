f56c0acbb21676127d22404aa4d0b7a2bf1e2ce6
Alaukik Pant <alaukikn.pant@gmail.com>
2022-08-23 13:13:49 +0800

    Add auth middleware

--- functions/src/middleware/index.ts
+++ functions/src/middleware/index.ts
@@ -1,6 +1,7 @@
 import {Request, Response, NextFunction} from "express";
 import * as functions from "firebase-functions";
 import createHttpError from "http-errors";
+import admin from "firebase-admin";
 
 export function logMiddleware(req: Request, _: Response, next: NextFunction) {
   functions.logger.log({path: req.path, req: req.body});
@@ -16,3 +17,35 @@ export function errorMiddleware(err: Error, req: Request, res: Response, _: Next
     res.status(500).json(err);
   }
 }
+
+export async function validateFirebaseIdToken(req: Request, res: Response, next: NextFunction) {
+  if ((!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) &&
+        !(req.cookies && req.cookies.__session)) {
+    res.status(403).send("Unauthorized");
+    return;
+  }
+
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else if (req.cookies) {
+    // Read the ID Token from cookie.
+    idToken = req.cookies.__session;
+  } else {
+    // No cookie
+    res.status(403).send("Unauthorized");
+    return;
+  }
+
+  try {
+    await admin.auth().verifyIdToken(idToken);
+    next();
+    return;
+  } catch (error) {
+    res.status(403).send("Unauthorized: Error while verifying Firebase ID token");
+    return;
+  }
+}
+
+
