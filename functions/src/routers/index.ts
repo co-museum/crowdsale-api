fc0a7db9aa507d23b245aa09218af1b44339989d
Uros Perisic <uros.m.perisic@gmail.com>
2022-09-06 18:03:17 +0800

    Add cors middleware and remove middleware module

--- functions/src/routers/index.ts
+++ functions/src/routers/index.ts
@@ -1,9 +1,31 @@
-import express, {Router} from "express";
+import express, {Router, Request, Response, NextFunction} from "express";
 import {Auth} from "firebase-admin/auth";
 import {Firestore} from "firebase-admin/firestore";
-import {errorMiddleware, logMiddleware} from "../middleware";
 import getAdminRouter from "./admin";
 import getClientRouter from "./client";
+import * as functions from "firebase-functions";
+import createHttpError from "http-errors";
+import {StatusCodes} from "http-status-codes";
+
+function logMiddleware(req: Request, _: Response, next: NextFunction) {
+  functions.logger.log({path: req.path, req: req.body});
+  next();
+}
+
+function corsMiddleware(_: Request, res: Response, next: NextFunction) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function errorMiddleware(err: Error, req: Request, res: Response, _: NextFunction) {
+  functions.logger.error({err: err, path: req.path, req: req.body});
+  if (createHttpError.isHttpError(err)) {
+    res.status(err.statusCode).json(err);
+  } else {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+  }
+}
 
 // NOTE: db and auth are injectable to enable mocking
 export default function getRouter(db: Firestore, auth: Auth): Router {
@@ -11,6 +33,7 @@ export default function getRouter(db: Firestore, auth: Auth): Router {
 
   router.use(express.json());
   router.use(logMiddleware);
+  router.use(corsMiddleware);
 
   router.use("/admin", getAdminRouter(db, auth));
   router.use("/client", getClientRouter(db));
